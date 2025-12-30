dac2fa7a0559e8be5af90b80ac63396c8c3e264d
manthan-chauhan-17 <manthanchauhan062@gmail.com>
2025-12-30 16:44:12 +0530

    Home Screen with BLoC , Add Item UI, DateHelper

--- lib/features/home/presentation/widgets/no_expiring_soon_items_found.dart
+++ lib/features/home/presentation/widgets/no_expiring_soon_items_found.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_pantry_frontend/core/theme/app_theme.dart';
+import 'package:pocket_pantry_frontend/core/utils/responsive.dart';
+import 'package:pocket_pantry_frontend/core/widgets/custom_text.dart';
 
 class NoExpiringSoonItemsFound extends StatelessWidget {
   const NoExpiringSoonItemsFound({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SizedBox(
+      height: 0.28 * Responsive.getHeight(context),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Some Image
+          CustomText(
+            'All Clear!',
+            fontSize: 20 * Responsive.getResponsiveText(context),
+            fontWeight: FontWeight.w600,
+            color: AppTheme.getColor(context).onSurface,
+          ),
+          SizedBox(height: 0.01 * Responsive.getHeight(context)),
+          CustomText(
+            'Great Job! , You have no items expiring soon. Your pantry is looking good!',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
